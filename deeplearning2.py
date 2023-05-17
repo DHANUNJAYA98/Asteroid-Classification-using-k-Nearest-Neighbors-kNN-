247aa4a62023edea4547a6e7f098f8a37efaabda
DHANUNJAYA98 <123298727+DHANUNJAYA98@users.noreply.github.com>
2023-05-17 11:52:53 +0530

    Add files via upload

---
+++ deeplearning2.py
@@ -0,0 +1,576 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+
+
+# Models
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix, classification_report, accuracy_score
+from sklearn.model_selection import GridSearchCV
+
+# Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import tree
+
+# Other
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# Allow us to use tensorflow
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam, SGD
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+
+# In[3]:
+
+
+import pandas as pd
+
+original = pd.read_csv("D:\dataset\dataset.csv")
+df=original.copy()
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[5]:
+
+
+df.info()
+
+
+# In[6]:
+
+
+nan_counts = df.isnull().sum()
+
+# filter the nan_counts Series to include only columns with non-zero NaN counts
+nan_counts_filtered = nan_counts[nan_counts > 0]
+
+# create a bar chart of the filtered NaN counts
+plt.figure(figsize=(18, 7))
+nan_counts_filtered.plot(kind='bar')
+plt.title('Number of NaN Values by Column')
+plt.xlabel('Columns')
+plt.ylabel('Number of NaN Values')
+plt.show()
+
+
+# In[7]:
+
+
+missing_cols = df.isna().mean() * 100
+missing_cols = missing_cols[missing_cols > 0]
+print("Percentage of missing values:\n", missing_cols)
+
+
+# In[8]:
+
+
+from scipy import stats
+# Check for missing values in the "H" attribute
+missing = df['H'].isna()
+
+# Prepare the data for Little's MCAR test
+observed = np.array([sum(~missing), sum(missing)])
+expected = np.array([len(df) * (1 - np.mean(missing)), len(df) * np.mean(missing)])
+
+# Conduct the test and calculate the p-value
+chi2, p_value = stats.chisquare(observed, f_exp=expected)
+print(p_value)
+# Interpret the results
+if p_value < 0.05:
+    print("The missing data mechanism for 'H' is not MCAR.")
+else:
+    print("The missing data mechanism for 'H' is MCAR.")
+
+
+# In[9]:
+
+
+# Check for missing values in the "diameter" attribute
+missing = df['diameter'].isna()
+
+# Prepare the data for Little's MCAR test
+observed = np.array([sum(~missing), sum(missing)])
+expected = np.array([len(df) * (1 - np.mean(missing)), len(df) * np.mean(missing)])
+
+# Conduct the test and calculate the p-value
+chi2, p_value = stats.chisquare(observed, f_exp=expected)
+print(p_value)
+# Interpret the results
+if p_value < 0.05:
+    print("The missing data mechanism for 'diameter' is not MCAR.")
+else:
+    print("The missing data mechanism for 'diameter' is MCAR.")
+
+
+# In[10]:
+
+
+# Check for missing values in the "albedo" attribute
+missing = df['albedo'].isna()
+
+# Prepare the data for Little's MCAR test
+observed = np.array([sum(~missing), sum(missing)])
+expected = np.array([len(df) * (1 - np.mean(missing)), len(df) * np.mean(missing)])
+
+# Conduct the test and calculate the p-value
+chi2, p_value = stats.chisquare(observed, f_exp=expected)
+print(p_value)
+# Interpret the results
+if p_value < 0.05:
+    print("The missing data mechanism for 'albedo' is not MCAR.")
+else:
+    print("The missing data mechanism for 'albedo' is MCAR.")
+
+
+# In[11]:
+
+
+# Check for missing values in the "diameter_sigma" attribute
+missing = df['diameter_sigma'].isna()
+
+# Prepare the data for Little's MCAR test
+observed = np.array([sum(~missing), sum(missing)])
+expected = np.array([len(df) * (1 - np.mean(missing)), len(df) * np.mean(missing)])
+
+# Conduct the test and calculate the p-value
+chi2, p_value = stats.chisquare(observed, f_exp=expected)
+print(p_value)
+# Interpret the results
+if p_value < 0.05:
+    print("The missing data mechanism for 'diameter_sigma' is not MCAR.")
+else:
+    print("The missing data mechanism for 'diameter_sigma' is MCAR.")
+
+
+# In[12]:
+
+
+# Remove the column which will not facilitate the analysis
+no_null_data=df.drop(['pdes', 'name', 'prefix', 'diameter', 'albedo', 'diameter_sigma'], axis=1)
+
+# Remove the row that includes null value
+no_null_data=no_null_data.dropna().reset_index(drop=True)
+
+
+# In[13]:
+
+
+no_null_data['spkid'] = no_null_data['spkid'].astype(str)
+
+
+# In[14]:
+
+
+df['sigma_i'].isnull().sum()
+
+
+# In[15]:
+
+
+# Figure with two subplots
+fig, axs = plt.subplots(ncols=2, figsize=(10, 5))
+
+#  Count plot of the "neo" variable on the left subplot
+sns.countplot(x='neo', data=df, ax=axs[0])
+axs[0].set_title('Near Earth Objects')
+
+# Add percentage labels to the left subplot
+total = float(len(df.neo))
+for p in axs[0].patches:
+    percentage = '{:.1f}%'.format(100 * p.get_height()/total)
+    x = p.get_x() + p.get_width() / 2 - 0.1
+    y = p.get_y() + p.get_height() + 3
+    axs[0].annotate(percentage, (x, y))
+
+#  Count plot of the "pha" variable on the right subplot
+sns.countplot(x='pha', data=df, ax=axs[1])
+axs[1].set_title('Potentially Hazardous Asteroids')
+
+# Add percentage labels to the right subplot
+total = float(len(df.pha))
+for p in axs[1].patches:
+    percentage = '{:.1f}%'.format(100 * p.get_height()/total)
+    x = p.get_x() + p.get_width() / 2 - 0.1
+    y = p.get_y() + p.get_height() + 3
+    axs[1].annotate(percentage, (x, y))
+
+# Adjust the spacing between the subplots
+fig.tight_layout()
+
+# Show the plot
+plt.show()
+
+# Count plot for Orbit Class
+fig, ax = plt.subplots(figsize=(12, 7))
+sns.countplot(x='class', data=df, ax=ax)
+plt.title('Orbit Class')
+
+
+plt.show()
+
+
+# In[48]:
+
+
+from sklearn.calibration import LabelEncoder
+
+# Remove identifying columns
+data=no_null_data.drop(['id', 'spkid', 'full_name', 'orbit_id', 'equinox'], axis=1).reset_index(drop=True)
+
+# Encode categorical features and target
+one_hot_encoded_data = pd.get_dummies(data, columns=['neo', 'class'])
+one_hot_encoded_data['pha'] = LabelEncoder(
+).fit_transform(one_hot_encoded_data['pha'])
+
+
+# In[17]:
+
+
+from sklearn.model_selection import train_test_split
+
+# Split train, validation, and test sets
+x = one_hot_encoded_data.drop('pha', axis=1)
+y = one_hot_encoded_data['pha'].to_frame()
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.4, random_state=100, stratify=y)
+x_val, x_test, y_val, y_test = train_test_split(
+    x_test, y_test, test_size=0.5, random_state=100, stratify=y_test)
+print("Shape of original dataset :", one_hot_encoded_data.shape)
+print("Shape of x_train set", x_train.shape)
+print("Shape of y_train set", y_train.shape)
+print("Shape of x_validation set", x_val.shape)
+print("Shape of y_validation set", y_val.shape)
+print("Shape of x_test set", x_test.shape)
+print("Shape of y_test set", y_test.shape)
+
+
+# In[18]:
+
+
+from sklearn.preprocessing import StandardScaler
+
+# Normalizing the features
+# Normalizing after splitting could prevent leaking information about the validation set into the train set
+# StandardScaler() is useful in classification and Normalizer() is useful in regression
+x_train = StandardScaler().fit_transform(x_train)
+x_val = StandardScaler().fit_transform(x_val)
+x_test = StandardScaler().fit_transform(x_test)
+
+
+# In[19]:
+
+
+y_train.value_counts()
+
+
+# In[20]:
+
+
+from imblearn.over_sampling import SMOTE
+
+# Data Upsampling - SMOTE
+x_train_us, y_train_us = SMOTE(
+    sampling_strategy=0.5, random_state=100).fit_resample(x_train, y_train)
+y_train_us.value_counts()
+
+
+# In[21]:
+
+
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+
+# Data Undersampling - Random Undersampling
+random_under_sampling = RandomUnderSampler(random_state=100)
+x_train_us_rus, y_train_us_rus = random_under_sampling.fit_resample(x_train_us, y_train_us)
+
+y_train_us_rus.value_counts()
+
+
+# In[22]:
+
+
+from imblearn.under_sampling import RandomUnderSampler
+
+# Data Undersampling - Random Undersampling
+random_under_sampling = RandomUnderSampler(random_state=100)
+x_train_us_rus, y_train_us_rus = random_under_sampling.fit_resample(x_train_us, y_train_us)
+
+y_train_us_rus.value_counts()
+
+
+# In[23]:
+
+
+from imblearn.over_sampling import SMOTE
+
+# Data Upsampling - SMOTE
+x_train_SMOTE, y_train_SMOTE = SMOTE(
+    sampling_strategy=0.5, random_state=100).fit_resample(x_train, y_train)
+y_train_SMOTE.value_counts()
+
+# Data Undersampling - Random Undersampling
+random_under_SAMPLING = RandomUnderSampler(random_state=100)
+x_train_us_UNDER, y_train_us_UNDER = random_under_SAMPLING.fit_resample(x_train_SMOTE, y_train_SMOTE)
+
+
+y_train_us_UNDER['pha'] = y_train_us_UNDER['pha'].map({0: 'N', 1: 'Y'})
+ax = sns.countplot(x="pha", data=y_train_us_UNDER)
+total = float(len(y_train_us_UNDER))
+for p in ax.patches:
+    height = p.get_height()
+    ax.text(p.get_x()+p.get_width()/2., height + 3, '{:.2f}%'.format(100*height/total), ha="center")
+plt.show()
+
+
+# In[24]:
+
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score, f1_score, classification_report
+
+
+rfc = RandomForestClassifier(class_weight='balanced', random_state=100)
+# Skip Hyperparameter Tuning part because parameter with dafult value get the highest accuracy of model
+
+rfc.fit(x_train_us_rus, y_train_us_rus)
+
+# Predict for validation set
+y_val_pred = rfc.predict(x_val)
+# Metrics
+precision_rfc, recall_rfc, fscore_rfc, support_rfc = precision_recall_fscore_support(
+    y_val, y_val_pred, average='macro')
+print(classification_report(y_val, y_val_pred))
+
+
+# In[25]:
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, precision_recall_fscore_support
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, recall_score, f1_score, classification_report
+lr = LogisticRegression()
+
+lr.fit(x_train_us_rus, y_train_us_rus)
+# Predict for validation set
+y_val_pred = lr.predict(x_val)
+
+# Metrics
+precision_lr, recall_lr, fscore_lr, support_lr = precision_recall_fscore_support(
+    y_val, y_val_pred, average='macro')
+print(classification_report(y_val, y_val_pred))
+
+
+# In[26]:
+
+
+dtc=DecisionTreeClassifier(class_weight='balanced', random_state=100)
+
+dtc.fit(x_train_us_rus, y_train_us_rus)
+# Predict for validation set
+y_val_pred=dtc.predict(x_val)
+
+# Metrics
+precision_dtc, recall_dtc, fscore_dtc, support_dtc = precision_recall_fscore_support(y_val, y_val_pred, average='macro')
+print(classification_report(y_val, y_val_pred))
+
+
+# In[27]:
+
+
+gnb=GaussianNB()
+
+gnb.fit(x_train_us_rus, y_train_us_rus)
+# Predict for validation set
+y_val_pred=gnb.predict(x_val)
+
+# Metrics
+precision_gnb, recall_gnb, fscore_gnb, support_gnb=precision_recall_fscore_support(y_val, y_val_pred, average='macro')
+print (classification_report(y_val, y_val_pred))
+
+
+# In[28]:
+
+
+from xgboost import XGBClassifier
+
+
+xgbc = XGBClassifier(max_depth=10, learning_rate=0.1,
+                     n_estimators=1000, eval_metric='mlogloss', random_state=100)
+
+# Train the model on the training set
+xgbc.fit(x_train, y_train)
+
+# Make predictions on the testing set
+y_pred = xgbc.predict(x_test)
+
+# Calculate precision, recall, and f1 score
+precision_xgbc = precision_score(y_test, y_pred)
+recall_xgbc = recall_score(y_test, y_pred)
+fscore_xgbc = f1_score(y_test, y_pred)
+
+# Print precision, recall, and f1 score
+print(f"Precision: {precision_xgbc:.2f}")
+print(f"Recall: {recall_xgbc:.2f}")
+print(f"F1 score: {fscore_xgbc:.2f}")
+
+# Print classification report
+print(classification_report(y_test, y_pred))
+
+
+# In[29]:
+
+
+pip install xgboost
+
+
+# In[30]:
+
+
+knc=KNeighborsClassifier(n_neighbors=1)
+knc.fit(x_train_us_rus, y_train_us_rus)
+# Predict for test set
+y_test_pred=knc.predict(x_test)
+
+# Metrics
+precision_knc, recall_knc, fscore_knc, support_knc=precision_recall_fscore_support(y_test, y_test_pred, average='macro')
+print(classification_report(y_test, y_test_pred))
+
+
+# In[31]:
+
+
+dl_model=Sequential()
+dl_model.add(Dense(10, activation='relu', input_shape=(45,)))
+dl_model.add(BatchNormalization())
+dl_model.add(Dense(200, activation='relu'))
+dl_model.add(BatchNormalization())
+dl_model.add(Dropout(0.05))
+dl_model.add(Dense(200, activation='relu'))
+dl_model.add(BatchNormalization())
+dl_model.add(Dense(1, activation='sigmoid'))
+
+
+# In[32]:
+
+
+dl_model.summary()
+
+
+# In[33]:
+
+
+dl_model.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+
+
+# In[34]:
+
+
+model_checkpoint_cb=ModelCheckpoint(filepath='MLP_model.h5',
+                                    save_best_only=True,
+                                    save_weights_only=True)
+early_stopping_cb=EarlyStopping(monitor='val_loss', 
+                                mode='min',
+                                verbose=1, 
+                                patience=15, 
+                                restore_best_weights=True)
+reducelr_on_plateau_cb=ReduceLROnPlateau(factor=0.05, patience=3)
+
+
+# In[40]:
+
+
+dl_model_history=dl_model.fit(x_train_us_rus,y_train_us_rus, 
+                              epochs=30, 
+                              batch_size=110, 
+                              validation_data=(x_val,y_val),
+                              callbacks=[model_checkpoint_cb, early_stopping_cb, reducelr_on_plateau_cb])
+
+
+# In[45]:
+
+
+# Evaluate traning history 
+plt.figure(figsize=(8,5))
+plt.plot(dl_model_history.history['accuracy'])
+plt.plot(dl_model_history.history['val_accuracy'])
+plt.legend(['Train Accuracy', 'Validation Accuracy'])
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Taining History')
+plt.savefig('MLP_model_training_history.png')
+
+
+# In[46]:
+
+
+# Evaluate loss history
+plt.figure(figsize=(8,5))
+plt.plot(dl_model_history.history['loss'])
+plt.plot(dl_model_history.history['val_loss'])
+plt.legend(['Train Loss', 'Validation Loss'])
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Loss History')
+plt.savefig('MLP_model_loss_history.png')
+
+
+# In[47]:
+
+
+# Predict for validation set
+y_val_pred = dl_model.predict(x_val)
+precision_dl, recall_dl, fscore_dl, support_dl=precision_recall_fscore_support(y_val, y_val_pred.round(), average='macro')
+print(classification_report(y_val,y_val_pred.round()))
+
+
+# In[44]:
+
+
+# Model Comparsion
+Model_Selection=pd.DataFrame({'Model':['Random Forest', 'XGBoost', 'Decision Tree', 'Navie Bayes', 'Logistic Regression', 'K-Nearest Neighbors', 'MLP Model'],
+                              'Precision':[precision_rfc, precision_xgbc, precision_dtc, precision_gnb, precision_lr,  precision_knc, precision_dl],
+                              'Recall':[recall_rfc, recall_xgbc, recall_dtc, recall_gnb, recall_lr,  recall_knc, recall_dl],
+                              'F1 Score':[fscore_rfc, fscore_xgbc, fscore_dtc, fscore_gnb, fscore_lr,  fscore_knc, fscore_dl],
+                              })
+Model_Selection.index+=1
+Model_Selection
+
+
+# In[ ]:
+
+
+
+
